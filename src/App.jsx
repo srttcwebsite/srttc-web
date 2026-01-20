6c39af83dc5595a18e03acbfdc10eb4c95a6b822
Chaitanya Golhar <golhar.241164@srttc.ac.in>
2026-01-20 16:38:18 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 // Import Page Template
 import DepartmentPage from './pages/DepartmentPage.jsx';
 
